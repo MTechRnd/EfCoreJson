53f45cd4a658ab01a6324b41ab65c01afcea3f4c
smitesh-maniya <123544692+smitesh-maniya@users.noreply.github.com>
2023-05-11 15:54:38 +0530

    read-csv-to-sql

---
+++ EFCoreJsonApp/Migrations/20230511052227_read csv file of orders.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper;
+using EFCoreJsonApp.Models.CsvDataReadModels;
+using FastMember;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using System.Globalization;
+
+#nullable disable
+
+namespace EFCoreJsonApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class readcsvfileoforders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            string path = "./files/orders.csv";
+            using var reader = new StreamReader(path);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csv.GetRecords<CsvOrderEntity>();
+            IConfiguration config = new ConfigurationBuilder()
+               .AddUserSecrets<Program>()
+               .Build();
+            using var connection = new SqlConnection(config.GetConnectionString("DefaultConnection"));
+            connection.Open();
+            using var transaction = connection.BeginTransaction();
+            using var bulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.Default, transaction);
+
+            bulkCopy.DestinationTableName = "Orders";
+            bulkCopy.ColumnMappings.Add("CustomerName", "CustomerName");
+            bulkCopy.ColumnMappings.Add("OrderDate", "OrderDate");
+
+            using var recordReader = ObjectReader.Create(records, "CustomerName", "OrderDate");
+            bulkCopy.WriteToServer(recordReader);
+
+            transaction.Commit();
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ EFCoreJsonApp/Models/CsvDataReadModels/CsvOrderDetailsWithJsonEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace EFCoreJsonApp.Models.CsvDataReadModels
+{
+    public class CsvOrderDetailsWithJsonEntity
+    {
+        public string CustomerName { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderDetailsJson { get; set; }
+    }
+}
