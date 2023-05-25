ec808b1ddd981f5dd22c0a9df0d264861a6e83b9
smitesh-maniya <123544692+smitesh-maniya@users.noreply.github.com>
2023-05-25 19:24:00 +0530

    mybenchmark modified

--- EFCoreJsonApp/MyBenchmark.cs
+++ EFCoreJsonApp/MyBenchmark.cs
@@ -1,62 +1,107 @@
 ﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
 using EFCoreJsonApp.Data;
 using EFCoreJsonApp.Services;
 using EFCoreJsonApp.Services.JsonService;
+using EFCoreJsonApp.Services.JsonUsingLinqService;
 using EFCoreJsonApp.Services.TraditionalService;
 
 namespace EFCoreJsonApp
 {
     [MemoryDiagnoser]
+    [Orderer(SummaryOrderPolicy.FastestToSlowest)]
     public class MyBenchmark
     {
         private ITraditionalService _traditionalService;
         private IJsonService _jsonService;
+        private IJsonUsingLinqService _jsonUsingLinqService;
         private DataContext _dataContext;
         private JsonDataContext _jsonDataContext;
         private List<Guid> _guidsOfTraditional;
         private List<Guid> _guidsOfJson;
+        private Guid _guidTraditional;
+        private Guid _guidJson;
 
         [GlobalSetup]
         public void Setup()
         {
+            _dataContext = new DataContext();
             _traditionalService = new TraditionalService(_dataContext);
+            _jsonDataContext =new JsonDataContext();
             _jsonService = new JsonService(_jsonDataContext);
+            _jsonUsingLinqService = new JsonUsingLinqService(_jsonDataContext);
             _guidsOfTraditional = new List<Guid>
             {
-                new Guid("8aed95bc-c0ef-ed11-9f03-f46b8c8f0ef6"),
-                new Guid("8eed95bc-c0ef-ed11-9f03-f46b8c8f0ef6"),
-                new Guid("5ef095bc-c0ef-ed11-9f03-f46b8c8f0ef6"),
-                new Guid("62f095bc-c0ef-ed11-9f03-f46b8c8f0ef6")
+                new Guid("bd031315-41f5-ed11-9f05-f46b8c8f0ef6"),
+                new Guid("190c1315-41f5-ed11-9f05-f46b8c8f0ef6"),
+                new Guid("421b1315-41f5-ed11-9f05-f46b8c8f0ef6")
             };
             _guidsOfJson = new List<Guid>
             {
-                new Guid("73dd068a-91f0-ed11-9f03-f46b8c8f0ef6"),
-                new Guid("a6dd068a-91f0-ed11-9f03-f46b8c8f0ef6"),
-                new Guid("badd068a-91f0-ed11-9f03-f46b8c8f0ef6"),
-                new Guid("9cdd068a-91f0-ed11-9f03-f46b8c8f0ef6")
+                new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
+                new Guid("977827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
+                new Guid("708b27d2-19fa-ed11-9f08-f46b8c8f0ef6")
             };
+            _guidTraditional = new Guid("09fa1215-41f5-ed11-9f05-f46b8c8f0ef6");
+            _guidJson = new Guid("127527d2-19fa-ed11-9f08-f46b8c8f0ef6");
         }
 
         [Benchmark(Baseline = true)]
-        public void TraditionalBenchmark()
+        public async Task TraditionalBenchmark()
         {
-            var res1 = _traditionalService.GetAllData();
-            //var res2 = _traditionalService.GetDataForSingleCustomer(new Guid("8bed95bc-c0ef-ed11-9f03-f46b8c8f0ef6"));
-            //var res3 = _traditionalService.GetDataForMultipleCustomer(_guidsOfTraditional);
-            //var res4 = _traditionalService.AggregateOperation();
-            //var res5 = _traditionalService.TotalOrdersOfCustomer(new Guid("8eed95bc-c0ef-ed11-9f03-f46b8c8f0ef6"));
-            //var res6 = _traditionalService.TotalOrdersOfCustomers();
+            //var res1 = await _traditionalService.GetAllDataAsync();
+            //var res2 = await _traditionalService.GetDataForSingleCustomerAsync(_guidTraditional);
+            //var res3 = await _traditionalService.GetDataForMultipleCustomerAsync(_guidsOfTraditional);
+            //var res4 = await _traditionalService.TotalOrdersOfCustomerAsync(_guidTraditional);
+            var res5 = await _traditionalService.TotalOrdersOfCustomersAsync();
+            //var res6 = await _traditionalService.AverageOfPriceAsync();
+            //var res7 = await _traditionalService.AverageOfQuantityAsync();
+            //var res8 = await _traditionalService.SumOfAllPriceAsync();
+            //var res9 = await _traditionalService.SumOfAllQuantityAsync();
+            //var res10 = await _traditionalService.GetMaxQuantityByOrderIdAsync(_guidTraditional);
+            //var res11 = await _traditionalService.GetMinQuantityByOrderIdAsync(_guidTraditional);
+            //var res12 = await _traditionalService.GetTotalByOrderIdAsync(_guidTraditional);
+            //var res13 = await _traditionalService.GetMaxPriceByOrderIdAsync(_guidTraditional);
+            //var res14 = await _traditionalService.GetMinPriceByOrderIdAsync(_guidTraditional);
         }
 
         [Benchmark]
-        public void JsonBenchmark()
+        public async Task JsonRawQueryBenchmark()
         {
-            var resJson1 = _jsonService.GetAllData();
-            //var resJson2 = _jsonService.GetDataForSingleCustomer(new Guid("73dd068a-91f0-ed11-9f03-f46b8c8f0ef6"));
-            //var resJson3 = _jsonService.GetDataForMultipleCustomer(_guidsOfJson);
-            //var resJson4 = _jsonService.AggregateOperation();
-            //var resJson5 = _jsonService.TotalOrdersOfCustomer(new Guid("badd068a-91f0-ed11-9f03-f46b8c8f0ef6"));
-            //var resJson6 = _jsonService.TotalOrdersOfCustomers();
+            //var resJson1 = await _jsonService.GetAllDataAsync();
+            //var resJson2 = await _jsonService.GetDataForSingleCustomerAsync(_guidJson);
+            //var resJson3 = await _jsonService.GetDataForMultipleCustomerAsync(_guidsOfJson);
+            //var resJson4 = await _jsonService.TotalOrdersOfCustomerAsync(_guidJson);
+            var resJson5 = await _jsonService.TotalOrdersOfCustomersAsync();
+            //var resJson6 = await _jsonService.AverageOfPriceAsync();
+            //var resJson7 = await _jsonService.AverageOfQuantityAsync();
+            //var resJson8 = await _jsonService.SumOfAllPriceAsync();
+            //var resJson9 = await _jsonService.SumOfAllQuantityAsync();
+            //var resJson10 = await _jsonService.GetMaxQuantityByOrderIdAsync(_guidJson);
+            //var resJson11 = await _jsonService.GetMinQuantityByOrderIdAsync(_guidJson);
+            //var resJson12 = await _jsonService.GetTotalByOrderIdAsync(_guidJson);
+            //var resJson13 = await _jsonService.GetMaxPriceByOrderIdAsync(_guidJson);
+            //var resJson14 = await _jsonService.GetMinPriceByOrderIdAsync(_guidJson);
+        }
+
+        [Benchmark]
+        public async Task JsonLinqBenchmark()
+        {
+            //var resJson1 = await _jsonUsingLinqService.GetAllDataAsync();
+            //var resJson2 = await  _jsonUsingLinqService.GetDataForSingleCustomerAsync(_guidJson);
+            //var resJson3 = await  _jsonUsingLinqService.GetDataForMultipleCustomerAsync(_guidsOfJson);
+            //var resJson4 =  _jsonUsingLinqService.TotalOrdersOfCustomerAsync(_guidJson);
+            var resJson5 = _jsonUsingLinqService.TotalOrdersOfCustomersAsync();
+            //var resJson6 =  _jsonUsingLinqService.AverageOfPriceAsync();
+            //var resJson7 =  _jsonUsingLinqService.AverageOfQuantityAsync();
+            //var resJson8 =  _jsonUsingLinqService.SumOfAllPriceAsync();
+            //var resJson9 = _jsonUsingLinqService.SumOfAllQuantityAsync();
+            //var resJson10 = await _jsonUsingLinqService.GetMaxQuantityByOrderIdAsync(_guidJson);
+            //var resJson11 = _jsonUsingLinqService.GetMinQuantityByOrderIdAsync(_guidJson);
+            //var resJson12 = _jsonUsingLinqService.GetTotalByOrderIdAsync(_guidJson);
+            //var resJson13 = _jsonUsingLinqService.GetMaxPriceByOrderIdAsync(_guidJson);
+            //var resJson14 = _jsonUsingLinqService.GetMinPriceByOrderIdAsync(_guidJson);
+
         }
     }
 }
--- EFCoreJsonApp/Program.cs
+++ EFCoreJsonApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using EFCoreJsonApp.Data;
+﻿using BenchmarkDotNet.Running;
+using EFCoreJsonApp.Data;
 using EFCoreJsonApp.Services;
 using EFCoreJsonApp.Services.JsonService;
 using EFCoreJsonApp.Services.JsonUsingLinqService;
@@ -12,7 +13,7 @@ namespace EFCoreJsonApp
     {
         private static async Task Main(string[] args)
         {
-            //BenchmarkRunner.Run<MyBenchmark>();
+            BenchmarkRunner.Run<MyBenchmark>();
             var hostBuilder = Host.CreateDefaultBuilder(args)
             .ConfigureServices((hostContext, services) =>
             {
@@ -28,14 +29,72 @@ namespace EFCoreJsonApp
             var serviceProvider = serviceScope.ServiceProvider;
 
             // Use services
-            var myService = serviceProvider.GetService<ITraditionalService>();
-            //var res = await myService.GetTotalByOrderId(new Guid("02fa1215-41f5-ed11-9f05-f46b8c8f0ef6"));
+            var traditionalService = serviceProvider.GetService<ITraditionalService>();
+            var id = new Guid("09fa1215-41f5-ed11-9f05-f46b8c8f0ef6");
+            var customerIds = new List<Guid>
+            {
+                new Guid("fcfb1215-41f5-ed11-9f05-f46b8c8f0ef6"),
+                new Guid("b6fd1215-41f5-ed11-9f05-f46b8c8f0ef6"),
+                new Guid("01fa1215-41f5-ed11-9f05-f46b8c8f0ef6")
+            };
+            Console.WriteLine("Traditional query:");
+            //var res1 = await traditionalService.GetAllDataAsync();
+            //var res2 = await traditionalService.GetDataForSingleCustomerAsync(id);
+            //var res3 = await traditionalService.GetDataForMultipleCustomerAsync(customerIds);
+            //var res4 = await traditionalService.TotalOrdersOfCustomerAsync(id);
+            //var res5 = await traditionalService.TotalOrdersOfCustomersAsync();
+            //var res6 = await traditionalService.AverageOfPriceAsync();
+            //var res7 = await traditionalService.AverageOfQuantityAsync();
+            //var res8 = await traditionalService.SumOfAllPriceAsync();
+            //var res9 = await traditionalService.SumOfAllQuantityAsync();
+            //var res10 = await traditionalService.GetMaxQuantityByOrderIdAsync(id);
+            //var res11 = await traditionalService.GetMinQuantityByOrderIdAsync(id);
+            //var res12 = await traditionalService.GetTotalByOrderIdAsync(id);
+            //var res13 = await traditionalService.GetMaxPriceByOrderIdAsync(id);
+            //var res14 = await traditionalService.GetMinPriceByOrderIdAsync(id);
 
+
+            Console.WriteLine("Json Raw Query:");
             var jsonService = serviceProvider.GetService<IJsonService>();
-            //var jsonResult = await jsonService.GetMinPriceByOrderId(new Guid("ffff068a-91f0-ed11-9f03-f46b8c8f0ef6"));
+            var idJson = new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6");
+            var customerIdsJson = new List<Guid>
+            {
+                new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
+                new Guid("977827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
+                new Guid("708b27d2-19fa-ed11-9f08-f46b8c8f0ef6")
+            };
+            //var resJson1 = await jsonService.GetAllDataAsync();
+            //var resJson2 = await jsonService.GetDataForSingleCustomerAsync(idJson);
+            //var resJson3 = await jsonService.GetDataForMultipleCustomerAsync(customerIdsJson);
+            //var resJson4 = await jsonService.TotalOrdersOfCustomerAsync(idJson);
+            var resJson5 = await jsonService.TotalOrdersOfCustomersAsync();
+            //var resJson6 = await jsonService.AverageOfPriceAsync();
+            //var resJson7 = await jsonService.AverageOfQuantityAsync();
+            //var resJson8 = await jsonService.SumOfAllPriceAsync();
+            //var resJson9 = await jsonService.SumOfAllQuantityAsync();
+            //var resJson10 = await jsonService.GetMaxQuantityByOrderIdAsync(idJson);
+            //var resJson11 = await jsonService.GetMinQuantityByOrderIdAsync(idJson);
+            //var resJson12 = await jsonService.GetTotalByOrderIdAsync(idJson);
+            //var resJson13 = await jsonService.GetMaxPriceByOrderIdAsync(idJson);
+            //var resJson14 = await jsonService.GetMinPriceByOrderIdAsync(idJson);
 
+            Console.WriteLine("Json Linq query:");
             var jsonLinqService = serviceProvider.GetService<IJsonUsingLinqService>();
-            //var josnResultLinq = await jsonLinqService.GetMinPriceByOrderId(new Guid("72dd068a-91f0-ed11-9f03-f46b8c8f0ef6"));
+            //var resJsonLinq1 = await jsonLinqService.GetAllDataAsync();
+            //var resJsonLinq2 = await jsonLinqService.GetDataForSingleCustomerAsync(idJson);
+            //var resJsonLinq3 = await jsonLinqService.GetDataForMultipleCustomerAsync(customerIdsJson);
+            //var resJsonLinq4 = await jsonLinqService.TotalOrdersOfCustomerAsync(new Guid("e5715528-43f5-ed11-9f05-f46b8c8f0ef6"));
+            //var resJsonLinq5 = await jsonLinqService.TotalOrdersOfCustomersAsync();
+            //var resJsonLinq6 = await jsonLinqService.AverageOfPriceAsync();
+            //var resJsonLinq7 = await jsonLinqService.AverageOfQuantityAsync();
+            //var resJsonLinq8 = await jsonLinqService.SumOfAllPriceAsync();
+            //var resJsonLinq9 = await jsonLinqService.SumOfAllQuantityAsync();
+            //var resJsonLinq10 = await jsonLinqService.GetMaxQuantityByOrderIdAsync(idJson);
+            //var resJsonLinq11 = await jsonLinqService.GetMinQuantityByOrderIdAsync(idJson);
+            //var resJsonLinq12 = await jsonLinqService.GetTotalByOrderIdAsync(idJson);
+            //var resJsonLinq13 = await jsonLinqService.GetMaxPriceByOrderIdAsync(idJson);
+            //var resJsonLinq14 = await jsonLinqService.GetMinPriceByOrderIdAsync(idJson);
+
         }
 
     }
