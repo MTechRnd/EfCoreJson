8a7d5f4d2c1fb84d80f617a7ce3d62d5b5f0bc82
smitesh-maniya <123544692+smitesh-maniya@users.noreply.github.com>
2023-06-28 18:35:07 +0530

    sqlCommands for table size

--- EFCoreJsonApp/Program.cs
+++ EFCoreJsonApp/Program.cs
@@ -45,7 +45,7 @@ namespace EFCoreJsonApp
                 new Guid("01fa1215-41f5-ed11-9f05-f46b8c8f0ef6")
             };
             Console.WriteLine("Traditional query:");
-            var res1 = await traditionalService.GetAllDataAsync();
+            //var res1 = await traditionalService.GetAllDataAsync();
             //var res2 = await traditionalService.GetDataForSingleCustomerAsync(id);
             //var res3 = await traditionalService.GetDataForMultipleCustomerAsync(customerIds);
             //var res4 = await traditionalService.TotalOrdersOfCustomerAsync(id);
@@ -134,11 +134,11 @@ namespace EFCoreJsonApp
 
             Console.WriteLine("Json Linq query:");
             var jsonLinqService = serviceProvider.GetService<IJsonUsingLinqService>();
-            var resJsonLinq1 = await jsonLinqService.GetAllDataAsync();
+            //var resJsonLinq1 = await jsonLinqService.GetAllDataAsync();
             //var resJsonLinq2 = await jsonLinqService.GetDataForSingleCustomerAsync(idJson);
             //var resJsonLinq3 = await jsonLinqService.GetDataForMultipleCustomerAsync(customerIdsJson);
             //var resJsonLinq4 = await jsonLinqService.TotalOrdersOfCustomerAsync(idJson);
-            //var resJsonLinq5 = await jsonLinqService.TotalOrdersOfCustomersAsync();
+            var resJsonLinq5 = await jsonLinqService.TotalOrdersOfCustomersAsync();
             //var resJsonLinq6 = await jsonLinqService.AverageOfPriceAsync();
             //var resJsonLinq7 = await jsonLinqService.AverageOfQuantityAsync();
             //var resJsonLinq8 = await jsonLinqService.SumOfAllPriceAsync();
@@ -230,15 +230,15 @@ namespace EFCoreJsonApp
 
 
             //Console.WriteLine("Json Raw Query:");
-            var jsonService = serviceProvider.GetService<IJsonService>();
-            var idJson = new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6");
-            var customerIdsJson = new List<Guid>
-            {
-                new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
-                new Guid("977827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
-                new Guid("708b27d2-19fa-ed11-9f08-f46b8c8f0ef6")
-            };
-            var resJson1 = await jsonService.GetAllDataAsync();
+            //var jsonService = serviceProvider.GetService<IJsonService>();
+            //var idJson = new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6");
+            //var customerIdsJson = new List<Guid>
+            //{
+            //    new Guid("7a7827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
+            //    new Guid("977827d2-19fa-ed11-9f08-f46b8c8f0ef6"),
+            //    new Guid("708b27d2-19fa-ed11-9f08-f46b8c8f0ef6")
+            //};
+            //var resJson1 = await jsonService.GetAllDataAsync();
             //var resJson2 = await jsonService.GetDataForSingleCustomerAsync(idJson);
             //var resJson3 = await jsonService.GetDataForMultipleCustomerAsync(customerIdsJson);
             //var resJson4 = await jsonService.TotalOrdersOfCustomerAsync(idJson);
